9ee0010b2c5d38d0fbc9d83637945a8259918e94
Driven Education <yessica_stern@hotmail.com>
2022-10-20 00:15:27 -0300

    search implementada, reset no styled components global e algumas libs instaladas

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "../style/style.js";
 import Main from "./Main";
 
 export default function App(){
     return(
         <>
+        <GlobalStyle/>
         <BrowserRouter>
         <Routes>
         <Route path="/main" element={<Main/>}/>
