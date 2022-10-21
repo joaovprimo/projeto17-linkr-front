caefa246fb1d5593c22538407ae90019f99dad52
JouJou <joujou@pop-os.localdomain>
2022-10-21 18:29:49 -0300

    Trending visuals

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -5,6 +5,7 @@ import UserContext from "../../context/UserContext";
 import { device } from "../../mediaqueries/devices";
 import { getPosts, postPublicate } from "../../services/linkr";
 import Post from "./Post.js";
+import Trending from "./Trending";
 
 export default function Timeline() {
   const [posts, setPosts] = useState([]);
@@ -17,7 +18,7 @@ export default function Timeline() {
 
   const { tasks, setTasks } = useContext(UserContext);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     const promisse = getPosts();
     promisse.then((res) => {
       setPosts(res.data);
@@ -28,7 +29,7 @@ export default function Timeline() {
         "An error occured while trying to fetch the posts, please refresh the page"
       )
     );
-  }, [posts]);
+  }, [posts]); */
 
   function handleNewPost(e) {
     setNewPost({ ...newPost, [e.target.name]: e.target.value });
@@ -106,7 +107,10 @@ export default function Timeline() {
           )}
         </div>
       </main>
-      <aside></aside>
+      <aside>
+        {" "}
+        <Trending />
+      </aside>
     </Wrapper>
   );
 }
@@ -188,6 +192,13 @@ const Wrapper = styled.div`
   display: block;
   margin: 3rem auto 0 auto;
   position: relative;
+
+  aside {
+    width: 35%;
+    position: absolute;
+    top: 55px;
+    right: 0;
+  }
 
   @media ${device.mobileM} {
     margin: 0;
