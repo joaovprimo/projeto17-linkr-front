65e87097827ad2c7fcd9c5a4b527cea73cb28f8c
vinicius <vinicius.coassis@gmail.com>
2022-10-22 08:17:33 -0300

    style: add responsive adjusts to timeline

--- src/components/timeline/Linkpreview.js
+++ src/components/timeline/Linkpreview.js
@@ -4,6 +4,8 @@ import { AiOutlineDelete } from "react-icons/ai";
 import { device } from "../../mediaqueries/devices";
 
 export default function LinkPreview({ urlInfo, url }) {
+  if(urlInfo === undefined) urlInfo = {title: "Link quebrado", description: "Link quebrado, refaça a postagem", canonical: "",image: "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png"}
+
   return (
     <Wrapper onClick={() => window.open(url)}>
       <div className="left">
@@ -27,7 +29,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   transition: all 0.5s;
-  max-width: 100%;
+  
 
   @media ${device.mobileM} {
     width: 100%;
@@ -46,35 +48,57 @@ const Wrapper = styled.div`
     border-top: 1px solid grey;
     border-left: 1px solid grey;
     border-bottom: 1px solid grey;
-    width: 100%;
+    max-width: 100%;
+    min-width: 65%;
+    overflow: hidden;
     border-radius: 6px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    @media ${device.mobileM} {
+      min-width: 70%;
+    }
   }
   h1 {
     font-size: 1.5rem !important;
     margin: 0;
     color: #cecece;
+    @media ${device.mobileM} {
+      font-size: 1rem !important;
+    }
   }
   h2 {
     font-size: 1.2rem;
     color: #9b9595;
+    @media ${device.mobileM} {
+      font-size: .8rem !important;
+    }
   }
   h3 {
     font-size: 1.2rem;
     color: #cecece;
+    max-width: 100%;
+    @media ${device.mobileM} {
+      font-size: .8rem !important;
+    }
   }
   .right {
     width: 25rem;
     height: 100%;
+    @media ${device.mobileM} {
+      width: 20rem;
+    }
 
     img {
       border-radius: 6px;
       margin-left: -1rem;
-      width: 100%;
+      min-width: 100%;
       height: 100%;
       object-fit: cover;
+      @media ${device.mobileM} {
+      object-fit: scale-down;
+    }
+      
     }
   }
 `;
